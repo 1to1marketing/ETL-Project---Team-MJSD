fd78498dc7255cce18073f62ea13890a28daa226
dante-mo <57079987+dante-m@users.noreply.github.com>
2020-01-18 11:40:51 -0600

    Update schema.sql

---
+++ schema.sql
@@ -0,0 +1,14 @@
+-- Create Two Tables
+CREATE TABLE Kaggle_CSV (
+  id INT PRIMARY KEY,
+  team_name TEXT,
+  year int,
+  attendance int,
+);
+
+CREATE TABLE MLB_API (
+  id INT PRIMARY KEY,
+  team_name TEXT,
+  city TEXT,
+  venue TEXT,
+);
